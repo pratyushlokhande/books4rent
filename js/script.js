aa07e0d799f18a50ffe6cac95001c4eb38492c8f
pratyushlokhande <pratyush3141@gmail.com>
2022-01-31 22:39:28 +0530

    success modal added

--- js/script.js
+++ js/script.js
@@ -35,6 +35,9 @@ window.addEventListener("load", function () {
   });
   gsap.from(".button-container", {translateY: 100, opacity: 0, duration: 1, ease: "power2.out", delay: 1.5});
   gsap.from("nav", {translateY: "-100%", duration: 1, ease: "power2.out", delay: 1.5});
+  gsap.from(".buy-sell", {translateY: 100, opacity: 0, duration: 1, ease: "power2.out", delay: 1.5});
+  gsap.from(".form-group", {opacity: 0, duration: 1, ease: "power2.out", delay: 2});
+  gsap.from(".address", {opacity: 0, duration: 1, ease: "power2.out", delay: 2});
 })
 
 // Smooth Scrolling
@@ -93,3 +96,26 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
 });
+
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let tl = gsap.timeline();
+
+  tl
+    .to(".modal", { translateY: "0%", duration: 1, ease: "Expo.easeInOut" })
+    .to(".modal-container", { translateY: "0%", opacity: 1, duration: 1, ease: "Expo.easeInOut" })
+})
+
+const close = document.querySelector("#close");
+
+close.addEventListener("click", (e) => {
+  document.getElementById("main-form").reset();
+  e.preventDefault();
+  let tl = gsap.timeline();
+  tl
+    .to(".modal-container", { translateY: "100%", opacity: 0, duration: 1, ease: "Expo.easeInOut" })
+    .to(".modal", { translateY: "-100%", duration: 1, ease: "Expo.easeInOut" });
+})
